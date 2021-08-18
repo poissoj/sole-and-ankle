8748e1ba15b3001940ce944f527eddea67f6629f
Jean-Marie Poissonnier <jmp@jm-it.fr>
2021-08-18 22:46:37 +0200

    Exercice 1: Superheader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -15,9 +15,9 @@ const SuperHeader = () => {
       </MarketingMessage>
       <SearchInput />
       <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
+      <StyledButton>
         <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
+      </StyledButton>
     </Wrapper>
   );
 };
@@ -26,20 +26,31 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  align-items: center;
+  height: 40px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
+  margin-left: 32px;
 `;
 
 const HelpLink = styled.a`
   color: inherit;
   text-decoration: none;
   outline-offset: 2px;
+  margin-left: 32px;
 
   &:not(:focus-visible) {
     outline: none;
   }
 `;
+
+const StyledButton = styled(UnstyledButton)`
+  margin-left: 32px;
+  margin-right: 32px;
+`
 
 export default SuperHeader;
